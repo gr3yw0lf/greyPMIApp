4be01ad1749399556d54b7444989d8b863e74092
Grey Whittney <grey@gr3yw0lf.com>
2013-07-17 09:32:51 -0400

    Added svg htaccess (just in case apache mime.types not set for it) Added simple diagram for moon phase image creation.

---
+++ moonSvg.php
@@ -0,0 +1,115 @@
+<?php
+header('Content-type: image/svg+xml');
+
+$xdoc = new DomDocument;
+$xdoc->Load('moonDiag.svg');
+
+$nodes = $xdoc->getElementsByTagName('path');
+
+$illum = 0.0;
+$age = 0.0;
+if (array_key_exists("illum",$_REQUEST)) {
+	$illum = $_REQUEST['illum'];
+	#print "illum set: $illum\n";
+}
+if (array_key_exists("age",$_REQUEST)) {
+	$age = $_REQUEST['age'];
+}
+$s=0.0;
+if (array_key_exists("s",$_REQUEST)) {
+	$s = $_REQUEST['s'];
+}
+
+if ($age>15) {
+	# this is past full moon
+	if ($illum<0.5) {
+
+		$rightArc=Array (
+			'color'=> 'black',
+			'translate'=> (200.0*$illum),
+            'scale'=> (-2.0*$illum)+1.0
+		);
+		$background['color'] = 'white';
+
+		$leftArc = Array (
+			'color' => 'black',
+		);
+
+	} else {
+		# illum > 0.5
+		$rightArc = Array (
+			'color' => "white"
+		);
+		$leftArc = Array (
+			'color' => 'white',
+			'translate'=> (-200.0*$illum)+200.0,
+			'scale'=> (2.0*$illum)-1.0
+		);
+	}
+} else {
+	# $age <15
+	# this is pre-full moon
+	if ($illum<0.5) {
+		$rightArc = Array (
+			'color' => "black"
+		);
+		$background['color'] = 'white';
+		$leftArc = Array (
+			'color' => 'black',
+			'translate'=> (200.0*$illum),
+			'scale'=> (-2.0*$illum)+1.0
+		);
+	} else {
+		#$illum > 0.5
+		$rightArc = Array (
+			'color' => "white",
+			'translate'=> (-200.0*$illum)+200.0,
+			'scale'=> (2.0*$illum)-1.0
+		);
+		$leftArc = Array (
+			'color' => 'white'
+		);
+	}
+}
+
+
+foreach($nodes as $arc) {
+	if ($arc->GetAttribute('id') == 'leftArc') {
+		#$transform = $arc->GetAttribute('transform');
+		if (array_key_exists('translate',$leftArc)) {
+			$arc->SetAttribute('transform',sprintf("translate(%s,0) scale(%s,1)",
+				$leftArc['translate'],
+				$leftArc['scale']
+			));
+		}
+		$arc->SetAttribute('style',sprintf("stroke: black; fill: %s",
+			$leftArc['color']
+		));
+	}
+	if ($arc->GetAttribute('id') == 'rightArc') {
+		#$transform = $arc->GetAttribute('transform');
+		if (array_key_exists('translate',$rightArc)) {
+			$arc->SetAttribute('transform',sprintf("translate(%s,0) scale(%s,1)",
+				$rightArc['translate'],
+				$rightArc['scale']
+			));
+		}
+		$arc->SetAttribute('style',sprintf("stroke: black; fill: %s",
+			$rightArc['color']
+		));
+	}
+}
+
+$nodes = $xdoc->getElementsByTagName('circle');
+
+foreach($nodes as $circle) {
+	if ($circle->GetAttribute('id') == 'background') {
+		$circle->SetAttribute('style',sprintf("stroke: black; fill: %s",
+			$background['color']
+		));
+	}
+}
+
+echo $xdoc->saveXML();
+
+?>
