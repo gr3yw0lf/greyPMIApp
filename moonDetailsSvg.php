228a046bafeee15cb8e663ebbcbbd396c1d8dfa3
Grey Whittney <grey@gr3yw0lf.com>
2013-07-24 21:34:02 -0400

    Added logic for the nes phase svg data

--- moonDetailsSvg.php
+++ moonDetailsSvg.php
@@ -25,10 +25,62 @@ if (array_key_exists("s",$_REQUEST)) {
 
 $cx = 150;
 $cy = 250;
-#$cx=100;
-#$cy=100;
 
-$nodes = $xdoc->getElementsByTagName('path');
+# from the moonSvg.php
+
+$background =Array ( 'color' => 'black' );
+
+if ($age<15) {
+	# this is pre full moon
+	if ($illum<0.5) {
+
+		$rightArc=Array (
+			'color'=> 'black',
+			'translate'=> ($cx*2*$illum),
+            'scale'=> (-2.0*$illum)+1.0
+		);
+		$background['color'] = 'white';
+
+		$leftArc = Array (
+			'color' => 'black',
+		);
+
+	} else {
+		# illum > 0.5
+		$rightArc = Array (
+			'color' => "white"
+		);
+		$leftArc = Array (
+			'color' => 'white',
+			'translate'=> ($cx*-2*$illum)+($cx*2),
+			'scale'=> (2.0*$illum)-1.0
+		);
+	}
+} else {
+	# $age >15
+	# this is post-full moon
+	if ($illum<0.5) {
+		$rightArc = Array (
+			'color' => "black"
+		);
+		$background['color'] = 'white';
+		$leftArc = Array (
+			'color' => 'black',
+			'translate'=> ($cx*2*$illum),
+			'scale'=> (-2.0*$illum)+1.0
+		);
+	} else {
+		#$illum > 0.5
+		$rightArc = Array (
+			'color' => "white",
+			'translate'=> ($cx*-2*$illum)+($cx*2),
+			'scale'=> (2.0*$illum)-1.0
+		);
+		$leftArc = Array (
+			'color' => 'white'
+		);
+	}
+}
 
 # calculate the new dx, and dy bassed on illumination of 0->1 for 0% to 100%
 # dx = 90*SIN($illum*2*pi()) + $cx
@@ -39,11 +91,10 @@ $dir = 0;
 if ($illum>0.5) {
 	$dir = 1;
 }
-
 #$newDxDy = Array( "$dx,$dy" );
-
 $end = (($illum*2.0)+1.5)*pi();
 
+$nodes = $xdoc->getElementsByTagName('path');
 foreach($nodes as $path) {
 	if ($path->GetAttribute('id') == 'path3760') { # the green arc
 		# need to extract d "M 150,160 ..... dx,dy"
@@ -59,10 +110,47 @@ foreach($nodes as $path) {
 		));
 		$path->SetAttribute('sodipodi:end',$end);
 	}
+	if ($path->GetAttribute('id') == 'leftArc') {
+		#$transform = $arc->GetAttribute('transform');
+		if (array_key_exists('translate',$leftArc)) {
+			$path->SetAttribute('transform',sprintf("translate(%s,0) scale(%s,1)",
+				$leftArc['translate'],
+				$leftArc['scale']
+			));
+		}
+		$path->SetAttribute('style',sprintf("stroke: black; fill: %s",
+			$leftArc['color']
+		));
+	}
+	if ($path->GetAttribute('id') == 'rightArc') {
+		#$transform = $arc->GetAttribute('transform');
+		if (array_key_exists('translate',$rightArc)) {
+			$path->SetAttribute('transform',sprintf("translate(%s,0) scale(%s,1)",
+				$rightArc['translate'],
+				$rightArc['scale']
+			));
+		}
+		$path->SetAttribute('style',sprintf("stroke: black; fill: %s",
+			$rightArc['color']
+		));
+	}
+	if ($path->GetAttribute('id') == 'arcArrowDec') {
+		$path->SetAttribute('transform',sprintf("rotate(%s,150,822)",
+			(360.0*$illum)
+		));
+	}
+}
+
+$nodes = $xdoc->getElementsByTagName('circle');
+foreach($nodes as $circle) {
+	if ($circle->GetAttribute('id') == 'background') {
+		$circle->SetAttribute('style',sprintf("stroke: black; fill: %s",
+			$background['color']
+		));
+	}
 }
 
 $nodes = $xdoc->getElementsByTagName('tspan');
-
 foreach($nodes as $tspan) {
 	if ($tspan->GetAttribute('id') == 'tspan3793') { 
 		$tspan->nodeValue = sprintf("%0.2f%%", $illum*100);
